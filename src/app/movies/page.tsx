f58350eb803f25b772eaf068ce98144d88195981
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-07-25 15:52:20 -0600

    first commit

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,10 @@
+import Waves from '@/components/Wave'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <Waves></Waves>
+        </div>
+    )
+}
